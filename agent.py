f759dfc6b74fe77d1eef0d808e9174b3c915fabc
= <=>
2023-03-10 11:16:59 +0530

    40%

--- agent.py
+++ agent.py
@@ -94,7 +94,7 @@ class Agent(PPO):
             #### Number of timesteps at the end of train may not be an exact multiple of save_intervals
             if int(self.num_timesteps/save_intervals) > i:
                 i = self.num_timesteps//save_intervals
-                savedir = "./model/"+dirname
+                savedir = "./models/"+dirname
                 filename = str(i)
                 path = savedir+filename
                 self.save(path)
@@ -113,7 +113,7 @@ dt = now.strftime("%d%m%Y%H%M%S")
 curr_dir = os.getcwd()
 model_name = "models/ppo_rect_long_debug/250"
 agent = Agent(model_load=os.path.join(curr_dir, model_name))
-agent.learn_model(150000000,"ppo_shortened_40_"+dt ,save_intervals=200000)
+agent.learn_model(50000000,"ppo_shortened_40_" + dt + "_" ,save_intervals=200000)
 # agent.learn_model(10000, save_intervals=3000)
 
 
